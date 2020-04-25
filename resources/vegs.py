17765038fc6760aeb2d91c7b47923b6980bc5cd1
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-25 14:45:43 -0500

    pretty sure creating new veg post route is working, like 9999999 percent

--- resources/vegs.py
+++ resources/vegs.py
@@ -1,17 +1,34 @@
 import models
 
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from playhouse.shortcuts import model_to_dict
 
 vegs = Blueprint('vegs', 'vegs')
 
 # dont forget!  this is GET route (/api/v1/vegs)
 @vegs.route('/', methods=['GET'])
 def vegs_index():
-	return 'vegs resource / blueprint working!'
+  result = models.Veg.select()
+  return 'get route hit'
 
 # CREATE
 # POST route /api/v1/vegs/ (don't forget slash at the end for POST)
 @vegs.route('/', methods=['POST'])
 def create_veg():
-  return "hitting CREATE route!"
+  payload = request.get_json()
+  print(payload)
+  new_veg = models.Veg.create(
+    name=payload['name'], 
+    color=payload['color'], 
+    isTasty=payload['isTasty']
+  )
+  print(new_veg)
+  print(new_veg.__dict__)
+  print(dir(new_veg))
+  veg_dict = model_to_dict(new_veg)
+  return jsonify(
+    data=veg_dict,
+    message="created(post) veg",
+    status=201
+  ), 201
 
