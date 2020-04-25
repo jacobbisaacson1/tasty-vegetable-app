dcc629de05bf9a6f7606272a41fd2fa969cd51ef
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-25 14:28:16 -0500

    hitting POST route for create veg

--- resources/vegs.py
+++ resources/vegs.py
@@ -4,6 +4,14 @@ from flask import Blueprint
 
 vegs = Blueprint('vegs', 'vegs')
 
-@vegs.route('/')
+# dont forget!  this is GET route (/api/v1/vegs)
+@vegs.route('/', methods=['GET'])
 def vegs_index():
 	return 'vegs resource / blueprint working!'
+
+# CREATE
+# POST route /api/v1/vegs/ (don't forget slash at the end for POST)
+@vegs.route('/', methods=['POST'])
+def create_veg():
+  return "hitting CREATE route!"
+
