fc3ec7bca29efda62a83dc0e3542584d0cf54261
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-28 22:38:02 -0500

    all users GET

--- resources/users.py
+++ resources/users.py
@@ -71,7 +71,7 @@ def login():
         message="email or password is incorrect",
         status=401
       ), 401
-      
+
   except models.DoesNotExist:
     print('username no good')
     return jsonify(
@@ -79,5 +79,17 @@ def login():
       message="email or password is incorrect",
       status=401
     ), 401
+
+@users.route('/all', methods=['GET'])
+def user_index():
+  users = models.User.select()
+  user_dicts = [ model_to_dict(user) for user in users ]
+  for user_dict in user_dicts:
+    user_dict.pop('password')
+  print(user_dicts)
+  return jsonify(user_dicts), 200
+
+
+
 
 
