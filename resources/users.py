9b8468d4ffab68b202ff78b364079aebf3f53827
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-28 21:19:54 -0500

    registering is working

--- resources/users.py
+++ resources/users.py
@@ -1,7 +1,12 @@
 import models
-from flask import Blueprint
+from flask import Blueprint, request
 users = Blueprint('users', 'users')
 
 @users.route('/', methods=['GET'])
 def test_user_resource():
 	return "user ref works"
+
+@users.route('/register', methods=['POST'])
+def register():
+	print(request.get_json())
+	return "see terminal"
