2c97fffa32b10e34d745559ac647d961d98421a3
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-28 21:34:44 -0500

    registration route being hit

--- resources/users.py
+++ resources/users.py
@@ -1,12 +1,32 @@
-import models
-from flask import Blueprint, request
-users = Blueprint('users', 'users')
+import models 
 
+from flask import Blueprint, request 
+from flask_bcrypt import generate_password_hash
+
+users = Blueprint('users', 'users')
 @users.route('/', methods=['GET'])
-def test_user_resource():
-	return "user ref works"
+def test_user_resource(): 
+  return "user resource works"
 
 @users.route('/register', methods=['POST'])
 def register():
-	print(request.get_json())
-	return "see terminal"
+  print(request.get_json())
+  payload = request.get_json()
+  payload['email'] = payload['email'].lower()
+  payload['username'] = payload['username'].lower()
+  print(payload)
+
+
+  try:
+    models.User.get(models.User.email == payload['email'])
+  except models.DoesNotExist:
+    pw_hash = generate_password_hash(payload['password'])
+    created_user = models.User.create(
+      username=payload['username'],
+      email=payload['email'],
+      password=pw_hash
+    )
+
+    print(created_user)
+
+  return "check terminal"
